13f95ca7b47e1d7225c6509ef78a84bcc07cd7bf
Ashton Fei <yunjia.fei@gmail.com>
2020-09-29 21:27:46 +0800

    complete the main forma and netlify folder

--- gas/app.js
+++ gas/app.js
@@ -1,11 +1,61 @@
-const getAppData = () => {
+const getAppData = (token) => {
+    const relations = getRelations()
+    const form = [
+        {
+            type: "text",
+            label: "Full name",
+            icon: "person",
+            required: true,
+            value: "",
+        },
+        {
+            type: "email",
+            label: "Email",
+            icon: "email",
+            required: true,
+            value: "",
+        },
+        {
+            type: "date",
+            label: "Date",
+            icon: "event",
+            required: true,
+            value: null,
+        },
+        {
+            type: "single",
+            label: "Gender",
+            icon: "people",
+            required: true,
+            value: null,
+            options: ["Male", "Female", "Other"],
+        },
+        {
+            type: "multiple",
+            label: "Languages",
+            icon: "language",
+            required: true,
+            value: [],
+            options: ["Chinese", "English", "Spanish", "Frech", "Japanese", "Catonese"],
+        },
+    ]
     const appData = {
         assets: ASSETS,
         app: {
             name: APP_NAME,
             description: APP_DESCRIPTION,
+            descriptionStranger: APP_DESCRIPTION_STRANGER,
+            confirmation: APP_CONFIRMATION,
         },
+        form,
+        relations,
+        user: null,
     }
+    if (!token) return JSON.stringify(appData)
+    const email = validateToken(token)
+    if (!email) return JSON.stringify(appData)
+    appData.user = getUserByEmail(email)
+    appData.user.token = token
     return JSON.stringify(appData)
 }
 
@@ -24,4 +74,13 @@ const signOut = (user) => {
     destroyToken(user.token)
     user = null
     return JSON.stringify(user)
+}
+
+
+const submitForm = (data) => {
+    let {values, headers} = JSON.parse(data)
+    values = [...values, new Date(), Utilities.getUuid()]
+    headers = [...headers, "Timestamp", "UUID"]
+    DB_RESPONSES.getRange(1,1,1,headers.length).setValues([headers])
+    DB_RESPONSES.appendRow(values)
 }
--- gas/common.js
+++ gas/common.js
@@ -1,15 +1,13 @@
 // consts
 const SPREADSHEET_ID = "183JylbkMxNpPqii21ljh3VPlDl-By2T6wTv4p8CGRTM" // the google spreadsheet id as the database for the app
-const APP_NAME = "CUSTOM FORM FOR SHAHBAZ" // the name of your application
-const APP_DESCRIPTION = "This is a a form built with Google Apps Script for Shahbaz, you need to sign in to use it." 
-
-// database sheet
-const DB_USERS = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName("Users")
-const KEYS_USERS = [
-    "email",
-    "password",
-    "fullname",
-]
+const APP_NAME = "Form with Login and MDD" // the name of your application
+const APP_DESCRIPTION = `<div>This is a form built with 
+    <a class="black-text" target="_blank" href="https://developers.google.com/apps-script/">Google Apps Script<\/a> & 
+    <a class="black-text" target="_blank" href="https://vuematerial.io/">VUE Material<\/a>.<\/div>` // HTML content supported
+const APP_DESCRIPTION_STRANGER = `<div>You need to sign in to use this form. Or you can make make a copy of the  
+    <a class="black-text" target="_blank" href="https://script.google.com/d/1FKXPUXoZZgLDtoE2DPV8_BeKW7-hLdYFgQ9gdn2kl1eu3tWihJSaB78T/edit?usp=sharing">Script<\/a> & the 
+    <a class="black-text" target="_blank" href="https://docs.google.com/spreadsheets/d/183JylbkMxNpPqii21ljh3VPlDl-By2T6wTv4p8CGRTM/copy">Spreadsheet<\/a> to start your own form.<\/div>` // HTML content supported
+const APP_CONFIRMATION = `<strong class="blue-text">Thanks for you submission.<\/strong>` // HTML content supported
 
 
 // labels and icons
@@ -27,6 +25,59 @@ const ASSETS = {
         icon: "person",
     }
 }
+
+// database sheet
+const DB_USERS = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName("Users")
+const KEYS_USERS = [
+    "email",
+    "password",
+    "fullname",
+]
+
+
+// form reseponse sheet
+const DB_RESPONSES = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName("Responses")
+const KEYS_RESPONSES = [
+    "compnay",
+    "region",
+    "contry",
+    "department",
+    "fullname",
+    "email",
+]
+
+const DB_RELATIONS = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName("Relations")
+function getRelations(){
+    const relations = []
+    const values = DB_RELATIONS.getDataRange().getValues()
+    const header = values.shift()
+    header.forEach( (header, index) => {
+        relations.push({
+            label: header,
+            icon: "filter_alt",
+            value: "",
+            required: true,
+            options: [],
+            kids: {},
+        })
+    })
+    values.forEach((value) => {
+        value.forEach((cell, index) =>{
+            if (relations[index].options.indexOf(cell) === -1) relations[index].options.push(cell)
+            if (index < value.length - 1) {
+                const kid = value[index + 1]
+                const kidKey = `${index}_kid_${cell}`
+                if (relations[index].kids[kidKey]) {
+                    if (relations[index].kids[kidKey].indexOf(kid) === -1) relations[index].kids[kidKey].push(kid)
+                }else{
+                    relations[index].kids[kidKey] = [kid]
+                }
+            }
+        })
+    })
+    return relations
+}
+
 
 /**
  * get user by email address
@@ -62,6 +113,11 @@ function createToken(email) {
     CacheService.getScriptCache().put(key, value, expirationInSeconds)
     return token
 }
+
+function validateToken(token) {
+    return CacheService.getScriptCache().get(token)
+}
+
 
 function destroyToken(token){
     CacheService.getScriptCache().remove(token)
